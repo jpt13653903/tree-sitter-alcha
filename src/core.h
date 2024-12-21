30a9b34bfdae4f9622f582a0c1e0d126af11d9df
John-Philip Taylor <jpt13653903@gmail.com>
2024-12-22 00:49:13 +0200

    Added some more built-in functions

--- src/core.h
+++ src/core.h
@@ -102,11 +102,49 @@ static void register_specials(TokenTree* token_tree)
 
 static void register_functions(TokenTree* token_tree)
 {
-    token_tree_insert(token_tree, "print",  BUILTIN_FUNC);
-    token_tree_insert(token_tree, "rose",   BUILTIN_FUNC);
-    token_tree_insert(token_tree, "fell",   BUILTIN_FUNC);
-    token_tree_insert(token_tree, "stable", BUILTIN_FUNC);
-    token_tree_insert(token_tree, "past",   BUILTIN_FUNC);
+    token_tree_insert(token_tree, "sin",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "cos",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "tan",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "asin",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "acos",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "atan",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "sinh",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "cosh",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "tanh",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "asinh",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "acosh",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "atanh",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "log",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "log2",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "log10",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "exp",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "pow",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "round",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "fix",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "floor",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "ceil",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "rand",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "real",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "imag",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "abs",        BUILTIN_FUNC);
+    token_tree_insert(token_tree, "angle",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "fft",        BUILTIN_FUNC);
+
+    token_tree_insert(token_tree, "read",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "write",      BUILTIN_FUNC);
+    token_tree_insert(token_tree, "append",     BUILTIN_FUNC);
+    token_tree_insert(token_tree, "textread",   BUILTIN_FUNC);
+    token_tree_insert(token_tree, "textwrite",  BUILTIN_FUNC);
+    token_tree_insert(token_tree, "textappend", BUILTIN_FUNC);
+    token_tree_insert(token_tree, "print",      BUILTIN_FUNC);
+
+    token_tree_insert(token_tree, "eval",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "shell",      BUILTIN_FUNC);
+
+    token_tree_insert(token_tree, "rose",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "fell",       BUILTIN_FUNC);
+    token_tree_insert(token_tree, "stable",     BUILTIN_FUNC);
+    token_tree_insert(token_tree, "past",       BUILTIN_FUNC);
 }
 //------------------------------------------------------------------------------
 
