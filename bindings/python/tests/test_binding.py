d37a218762c410a1dde75d7b98206c6905e1a57f
John-Philip Taylor <jpt13653903@gmail.com>
2025-09-20 10:25:43 +0200

    Regenerated

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
-import tree_sitter
+from tree_sitter import Language, Parser
 import tree_sitter_alcha
 
 
 class TestLanguage(TestCase):
     def test_can_load_grammar(self):
         try:
-            tree_sitter.Language(tree_sitter_alcha.language())
+            Parser(Language(tree_sitter_alcha.language()))
         except Exception:
             self.fail("Error loading Alcha grammar")
